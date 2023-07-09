16df95473bd5ae91f0cb01049dd0db0f5cc3fdfa
maheryfit <mahery_fitahiana@yahoo.com>
2023-07-09 14:17:55 +0300

    init

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url', 'file', 'string', 'cloture', 'number');
+$autoload['helper'] = array('url', 'file', 'string', 'number');
 
 /*
 | -------------------------------------------------------------------
