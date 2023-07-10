cb80ae3615ae874bd47176c89fa84fafba591569
NyAinaLoann <120193249+NyAinaLoann@users.noreply.github.com>
2023-07-10 14:39:15 +0300

    Correction (2) front-office

--- application/views/front-page/template-front.php
+++ application/views/front-page/template-front.php
@@ -1,7 +1,10 @@
 <?php
    defined('BASEPATH') OR exit('No direct script access allowed');
    $this->load->view("front-page/template-front/sidebar");
+<<<<<<< Updated upstream
    $this->load->view("front-page/template-front/navbar");
+=======
+>>>>>>> Stashed changes
    if (!empty($page)) {
       $this->load->view("front-page/" . $page);
    }
--- application/views/front-page/template-front/footer.php
+++ application/views/front-page/template-front/footer.php
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
           <footer class="footer">
             <div class="d-sm-flex justify-content-center justify-content-sm-between">
               <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © bootstrapdash.com 2020</span>
@@ -37,3 +38,37 @@
     <!-- End custom js for this page -->
   </body>
 </html>
+=======
+<footer>
+         <div class="footer">
+            <div class="container">
+               <div class="row">
+                  <div class="col-md-8 offset-md-2">
+                     <div class="cont">
+                        <h3> Free Multipurpose  <br> Responsive Landing Page 2019</h3>
+                        <p>Modern lighting fast & easily Customizable</p>
+                     </div>
+                     <button class="sub_btn" href="#">Get A Quote</button>
+                  </div>
+               </div>
+            </div>
+            <div class="copyright">
+               <div class="container">
+                  <div class="row">
+                     <div class="col-md-12">
+                        <p>Copyright 2019 All Right Reserved By <a href="https://html.design/"> Free Html Templates</a></p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+</footer>
+      <script src="<?=base_url()?>/assets/js/jquery.min.js"></script>
+      <script src="<?=base_url()?>/assets/js/popper.min.js"></script>
+      <script src="<?=base_url()?>/assets/js/bootstrap.bundle.min.js"></script>
+      <script src="<?=base_url()?>/assets/js/jquery-3.0.0.min.js"></script>
+      <!-- sidebar -->
+      <script src="<?=base_url()?>/assets/js/jquery.mCustomScrollbar.concat.min.js"></script>
+      <script src="<?=base_url()?>/assets/js/custom.js"></script>
+      <script src="<?=base_url()?>/assets/https:cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.js"></script>
+>>>>>>> Stashed changes
