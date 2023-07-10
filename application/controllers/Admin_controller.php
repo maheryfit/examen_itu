e1053dc6710cd4bbab1386641c0b1d708cfadd64
AntsaAnthony <antsarahaniraka@gmail.com>
2023-07-10 21:34:10 +0300

    update

--- application/controllers/Admin_controller.php
+++ application/controllers/Admin_controller.php
@@ -24,4 +24,20 @@ class Admin_controller extends CI_Controller {
         $this->data["page"] = "register";
         $this->load->view("admin-page/template-admin", $this->data);
     }
+
+    public function log(){
+        $this->load->model("Utilisateur");
+        $data = $this->input->post();
+
+        try {
+            $rep = $this->Utilisateur->checkLogin($data);
+        } catch (Exception $e) {
+            echo "Erreur : ".$e->getMessage();
+            // redirect(base_url("admin_controller/register"));
+        }
+
+        // redirect(base_url("index.php/admin_controller/index"));
+
+
+    }
 }
