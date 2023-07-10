9fd407ee174c8cbc24b29d090d03d5aa2600d7dd
AntsaAnthony <antsarahaniraka@gmail.com>
2023-07-10 16:04:51 +0300

    front-office

--- application/views/front-page/template-front.php
+++ application/views/front-page/template-front.php
@@ -1,10 +1,6 @@
 <?php
    defined('BASEPATH') OR exit('No direct script access allowed');
-<<<<<<< HEAD
    $this->load->view("front-page/template-front/navbar");
-=======
-   $this->load->view("front-page/template-front/sidebar");
->>>>>>> 3b62a42732a6b030f2dcdb08adf5b2edb72891ee
    if (!empty($page)) {
       $this->load->view("front-page/" . $page);
    }
