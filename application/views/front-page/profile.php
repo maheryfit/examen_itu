7176cd02e2afb1f6a40aede979189ebb0b64bedb
AntsaAnthony <antsarahaniraka@gmail.com>
2023-07-11 13:50:04 +0300

    update pdf

--- application/views/front-page/profile.php
+++ application/views/front-page/profile.php
@@ -9,6 +9,8 @@
                         <h3> Welcome <?=$session["nom"]?> !</h3>
                      <?php }
                   ?>
+
+                  <a href="<?=site_url('PdfController/index')?>">Test pdf</a>
                   <h1>Healthy Food Recipes</h1>
                   <p>There are many variations of passages of Lorem Ipsum available, but the majorityomised words which don't look even slightly believable</p>
                   <a class="read_more" href="#">Read More</a>
