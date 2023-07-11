3e5777b8f88b01ff339b709fb7f850ebf5e2ced8
NyAinaLoann <120193249+NyAinaLoann@users.noreply.github.com>
2023-07-11 16:31:49 +0300

    Front-Office

---
+++ application/views/front-page/suggestion.php
@@ -0,0 +1,30 @@
+<div class="col-lg-12 grid-margin stretch-card">
+                <div class="card">
+                  <div class="card-body">
+                    <h4 class="card-title"> Nous vous proposons les régimes suivants: </h4>
+                    
+                    <div class="table-responsive">
+                      <table class="table table-bordered">
+                        <thead>
+                          <tr>
+                            <th> # </th>
+                            <th> Nom régime </th>
+                            <th> Durée (en jours)</th>
+                            <th> Poids obtenu/perdu</th>
+                            <th> Montant </th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr>
+                            <td> 1 </td>
+                            <td> <a href="#"> Keto </a> </td>
+                            <td> 26 </td>
+                            <td> 5 </td>
+                            <td> 800000 </td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </div>
+                  </div>
+                </div>
+              </div>
