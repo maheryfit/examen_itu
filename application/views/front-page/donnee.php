4365c53d2a92a6c2f5abe56a6dc4d127ff57e474
NyAinaLoann <120193249+NyAinaLoann@users.noreply.github.com>
2023-07-11 16:21:18 +0300

    Update donnee.php

--- application/views/front-page/donnee.php
+++ application/views/front-page/donnee.php
@@ -3,22 +3,29 @@
       <!-- end banner -->
       <!-- food -->
         <div class="food">
-            <div class="container">
+            <div class="col-12 grid-margin stretch-card">
                 <h6> Veuillez entrer vos informations </h6>
                 <div>
-                    <p>
-                        Type de régime: <select>
-                                            <?php for ($i=0; $i < count($categorie) ; $i++) { ?>
-                                                <option value="<?php echo $categorie[$i]->get_id_categorie_regime();?>"> <?php echo $categorie[$i]->get_nom();?> </option>
-                                            <?php }?>
-                                        </select>
-                    </p>
-                    <p>
-                        Poids initiale: <input type="number" name="poidsI">
-                    </p>
-                    <p>
-                        Poids desiré: <input type="number" name="poids">
-                    </p>
+                    <div class="form-group">
+                        <p>
+                            Type de régime: <select>
+                                                <?php for ($i=0; $i < count($categorie) ; $i++) { ?>
+                                                    <option value="<?php echo $categorie[$i]->get_id_categorie_regime();?>"> <?php echo $categorie[$i]->get_nom();?> </option>
+                                                <?php }?>
+                                            </select>
+                        </p>
+                    </div>
+                    <div class="form-group">
+                        <p>
+                            Poids initiale: <input type="number" name="poidsI">
+                        </p>
+                    </div>
+                    <div class="form-group"> 
+                        <p>
+                            Poids desiré: <input type="number" name="poids">
+                        </p>
+                    </div>
+                    
                     <button> Devis </button>
                 </div>
             </div>
