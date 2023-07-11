e9ca64070ae983f61ab46e1c14dd5b8173f6200f
NyAinaLoann <120193249+NyAinaLoann@users.noreply.github.com>
2023-07-11 08:16:05 +0300

    Admin (3)

--- application/views/admin-page/insertion-sport.php
+++ application/views/admin-page/insertion-sport.php
@@ -1,17 +1,19 @@
-<div>
-    <h6> Insertion Activité Sportive </h6>
-    <form>
-        <div>
-            <p>
-                Type de régime: <select>
-                                    <option> Perdre du poids </option>
-                                    <option> Prendre du poids </option>
-                                </select>
-            </p>
-            <p>
-                Nom de l'exercice: <input type="text" name="nom">
-            </p>
-            <input type="button" name="ok" value="valider">
-        </div>
-    </form>
+<div class="main-panel">
+    <div class="content-wrapper">
+        <h6> Insertion Activité Sportive </h6>
+        <form>
+            <div>
+                <p>
+                    Type de régime: <select>
+                                        <option> Perdre du poids </option>
+                                        <option> Prendre du poids </option>
+                                    </select>
+                </p>
+                <p>
+                    Nom de l'exercice: <input type="text" name="nom">
+                </p>
+                <input type="button" name="ok" value="valider">
+            </div>
+        </form>
+    </div>
 </div>
